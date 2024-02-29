f1cfe58d8e7c3dd84654a47ca7aa2ad585cf077b
Roldan Ordaz Paez <roldanop@gmail.com>
2024-02-28 16:36:33 -0800

    module

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, us
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
+  // Middleware
+app.use(bodyParser.json());
+
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ utilities/data.js
@@ -0,0 +1,10 @@
+const users = [
+    {username: 'Robert', email:'robert@mail.com', age: 33 },
+    {username: 'Thomas', email:'Thomas@mail.com', age: 22 },
+    {username: 'Olivia', email:'olivia@mail.com', age: 30 },
+    {username: 'Amelia', email:'amel@mail.com', age: 40 },
+    {username: 'Michael', email:'mike@mail.com', age: 30 },
+
+];
+
+module.exports = users;
